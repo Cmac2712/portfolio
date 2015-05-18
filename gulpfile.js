be373d65794209ae50286c188ecab0584f62ef54
Craig <cmac2712@gmail.com>
2015-05-18 04:57:46 +0100

    Gulp and styling tweaks

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp   = require('gulp');
+var bs     = require('browser-sync');
+var reload = bs.reload;
+var sass   = require('gulp-sass');
+
+gulp.task('serve', function () {
+    bs.init({
+	server: {
+	    baseDir: "./"
+	}
+    }); 
+
+    gulp.watch("./styles/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', reload);
+    gulp.watch("./styles/*").on('change', reload);
+});
+
+gulp.task('sass', function () {
+      gulp.src('./styles/*.scss')
+	  .pipe(sass().on('error', sass.logError))
+	  .pipe(gulp.dest('./styles'));
+
+});
+
+gulp.task('default', ['serve']);
