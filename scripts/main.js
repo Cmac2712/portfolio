caf4f68926e764fc447a3bcbd0c091a93411c952
Craig <cmac2712@gmail.com>
2015-05-20 23:56:23 +0100

    More tidying

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,7 @@ requirejs(['ui'],
 	    
 	    /**
 	     *  A friendly message
-	    */
+	     */
 
 	console.log("%c Hello, thanks for taking the time to look under the hood. If you have any suggestions, criticisms or notice any bugs please email me at %ccmac2712@gmail.com", "color: #444; font-weight: bold;", "text-decoration: underline; font-weight: bold; color: #66e;")
 	});
