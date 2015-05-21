b75cecb2788ef35a7e3c41ee8dee7abf371acfc7
Craig <cmac2712@gmail.com>
2015-05-22 00:01:12 +0100

    Added new logos

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,6 @@ requirejs(['ui'],
 	     *  A friendly message
 	     */
 
-	console.log("%c Hello, thanks for taking the time to look under the hood. If you have any suggestions, criticisms or notice any bugs please email me at %ccmac2712@gmail.com", "color: #444; font-weight: bold;", "text-decoration: underline; font-weight: bold; color: #66e;")
+	console.log("Hello, thanks for taking the time to look under the hood. You can see the source at https://github.com/cmac2712/porfolio.\n If you have any suggestions, criticisms or notice any bugs please email me at cmac2712@gmail.com");
 	});
     
