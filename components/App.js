5084468f15871b14d534b0a910599aeb97997f07
Craig <craig.macintyre@indez.com>
2019-01-05 04:03:18 +0000

    Added GTM

--- components/App.js
+++ components/App.js
@@ -84,7 +84,14 @@ class App extends React.Component {
 					<title>{this.props.meta.title}</title>
 					<meta name="viewport" content="width=device-width, initial-scale=1"/>
 					<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600,700" rel="stylesheet" />
+					<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+					})(window,document,'script','dataLayer','GTM-WMQCK3W');</script>
 				</Head>
+				<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WMQCK3W"
+				height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 				<Header
 					{...this.props.sectionHeader}
 					ref={(section) => { this.headerRef = section }}
