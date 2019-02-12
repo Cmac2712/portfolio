65517cca26d3634e12fe77ccd3346c04c9c3ddf2
Craig <craig.macintyre@indez.com>
2019-02-12 16:24:02 +1100

    Created separate server for fetching info

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+const port = 8000;
+
+app.get('/api/data', (req, res) => {
+	res.sendFile(path.join(__dirname, 'data.json'));
+});
+
+app.listen(port, () => {
+	console.log(`API listening on port ${port}`);
+});
+
+
