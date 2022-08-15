dfc318668a5232235b9d6741b2e79d5fbfde0d48
Craig MacIntyre <craig@boundary.co.uk>
2022-08-15 21:17:23 +0100

    Fix build error

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,17 @@
 import React from 'react';
-import axios from 'axios';
 import App from '../components/App.js';
-//import '../scss/styles.scss';
-import Loading from '../components/Loading';
-
 
 export async function getStaticProps() {
   const res = await fetch('https://craigmacintyre.co.uk/api/data')
   const data = await res.json()
 
-  console.log('here we go');
-
   return {
     props: data
   }
 }
-class Index extends React.Component {
 
-	constructor(props) {
-		super(props)
-	}	
-	
-	render () {
-		return  <App {...this.props} />;
-	}
+const Index = props => {
+	return <Index {...props} />;
 }
 
 export default Index
