49d1ba2c4ce210bc7fe0bceb62409575ea0f945c
Craig MacIntyre <craig@boundary.co.uk>
2022-08-15 21:58:20 +0100

    Added Serverless Function

---
+++ src/pages/api/data.js
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import data from '../../../server/data.json'
+
+export default function handler(
+  request,
+  response
+) {
+  response.status(200).json({
+    body: data,
+    query: request.query,
+    cookies: request.cookies,
+  });
+}
