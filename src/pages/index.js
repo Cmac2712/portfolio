ae0afc664bab2b01dce244601242231a4643e36f
Craig MacIntyre <craig@boundary.co.uk>
2022-08-15 21:34:48 +0100

    SSG

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import axios from 'axios';
 import App from '../components/App.js';
+//import '../scss/styles.scss';
+import Loading from '../components/Loading';
 
-export async function getStaticProps() {
+
+export async function getServerSideProps() {
   const res = await fetch('https://craigmacintyre.co.uk/api/data')
-  const data = await res.json()
+  const props = await res.json()
 
   return {
-    props: data
+	props
   }
 }
+class Index extends React.Component {
 
-const Index = props => {
-	return <Index {...props} />;
+	constructor(props) {
+		super(props)
+	}	
+	
+	render () {
+		return  <App {...this.props} />;
+	}
 }
 
 export default Index
